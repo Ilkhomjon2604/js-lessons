f838590cad8a65b5bb15d88dfef45ffa31a4313f
Ilkhomjon2604 <m347es@gmail.com>
2023-06-16 11:53:40 +0200

    finding only Uppercased letters

--- 6th-lesson/app.js
+++ 6th-lesson/app.js
@@ -1,7 +1,11 @@
-let num = 17;
+let num = 5;
 
 for (let i = 1; i<= num; i++){
     (i % 2== 0) ? console.log( `${i} is even number`) : console.log( `${i} is odd number`);
 }
 
+let word = "WebBraiN";
+
+const onlyUpper2 = word.replace(/[^A-Z]/g, '');
+console.log(onlyUpper2);
 
