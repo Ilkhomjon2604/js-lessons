3461c756348e7848211ff289475b6db26b0dca90
Ilkhomjon2604 <m347es@gmail.com>
2023-06-24 23:18:07 +0200

    // N2 switch string value

--- 9th-lesson/app.js
+++ 9th-lesson/app.js
@@ -3,3 +3,16 @@ let checkLastLetter = (a) =>{
   return Boolean( a[a.length -1] == 'l' | a[a.length -1] == 'L')
 }
 console.log(checkLastLetter('FoziL'));
+
+// N2 switch string value 
+
+let switchValeu = (a, b) => {
+    let c = a;
+    a = b;
+    b = c;
+    console.log(a,b);
+    return a + b
+
+}   
+
+console.log(switchValeu('IT', 'Acedumy'));
