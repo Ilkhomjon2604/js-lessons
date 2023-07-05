be33c3cbbdc664714e78ecae51b08f1de49dd8d4
Ilkhomjon2604 <m347es@gmail.com>
2023-07-05 22:02:18 +0200

    task two isEmpty() function alert

--- 10th-lesson/app.js
+++ 10th-lesson/app.js
@@ -1,9 +1,20 @@
-let user = {};
-user.name = 'John'
-console.log(user);
-user.surename = 'Smith';
-console.log(user);
-user.name = 'Pate';
-console.log(user);
-delete user.name
-console.log(user);
+// let user = {};
+// user.name = 'John'
+// console.log(user);
+// user.surename = 'Smith';
+// console.log(user);
+// user.name = 'Pate';
+// console.log(user);
+// delete user.name
+// console.log(user);
+
+let schadule = {};
+
+let isEmpty = (a) =>  Object.keys(a).length ? false : true 
+
+alert( isEmpty(schadule))
+
+schadule['8:30'] = 'Get up'
+
+alert( isEmpty(schadule))
+
