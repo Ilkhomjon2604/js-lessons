eeb877947a61daa937580673bb5e74900191a0e9
Ilkhomjon2604 <m347es@gmail.com>
2023-07-07 21:29:42 +0200

    task 1 va task 2 is done

---
+++ object/app.js
@@ -0,0 +1,27 @@
+// / N1
+// object keylarini toping
+
+// let obj = {
+//     name : 'web',
+//     age : 10
+// }
+
+// for (const key in obj) {
+//     console.log(key);
+// }
+
+// no2
+// object valuelarining raqamga tenglarini toping
+
+// let obj = {
+//         name : 'web',
+//         age : 10
+//     }
+    
+//     for (const key in obj) {
+//         (typeof obj[key] == 'number') ? console.log(key): obj
+//     }
+
+
+    // No3
+// object valuelarining boolean tipidagi malumotlrini qaytaring 
