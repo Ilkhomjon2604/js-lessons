618ca26d40906e764ea7d5114dfe4477be71079b
Ilkhomjon2604 <m347es@gmail.com>
2023-07-14 19:53:34 +0200

    sort is done

--- array2/app.js
+++ array2/app.js
@@ -7,24 +7,35 @@
 
 
 // Task 2
-let cars = [
-    { id: 1 , year: 1998, engine: 1, name:'Tico' },
-    { id: 2 , year: 2005, engine: 1.2, name:'Matiz' },
-    { id: 3 , year: 2010, engine: 1.6, name:'Gentra' },
-    { id: 4 , year: 2010, engine: 1.5, name:'Cobalt' },
-    { id: 5 , year: 2012, engine: 2, name:'Malibu' },
-    { id: 6 , year: 2000, engine: 1.2, name:'Damas' },
-    { id: 7 , year: 2018, engine: 2.4, name:'Tracker' }
-];
+// let cars = [
+//     { id: 1 , year: 1998, engine: 1, name:'Tico' },
+//     { id: 2 , year: 2005, engine: 1.2, name:'Matiz' },
+//     { id: 3 , year: 2010, engine: 1.6, name:'Gentra' },
+//     { id: 4 , year: 2010, engine: 1.5, name:'Cobalt' },
+//     { id: 5 , year: 2012, engine: 2, name:'Malibu' },
+//     { id: 6 , year: 2000, engine: 1.2, name:'Damas' },
+//     { id: 7 , year: 2018, engine: 2.4, name:'Tracker' }
+// ];
+// filter ============================
 
 // let oldCars = cars.filter((value)=> value.year < 2000)
 // console.log(oldCars);
 
+// Add value ======================
 // console.log(cars.map((value)=> value["engine"].sort()));
 // cars.map((value)=> {
 //     if (value.year< 2005)  value.status = 'Old' })
 // console.log(cars);
 
+// Sort ++++++++++++++++++++++++++++++++==
+
+// const sortByYear = () =>{
+//     let res = cars.sort((a, b) => a.engine - b.engine);
+//     cars = res
+// };
+
+// sortByYear()
+// console.log(cars);
 
 
 
