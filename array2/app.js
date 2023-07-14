ecf8bb8dcda67a01d27de4c2b42e4b400526ff33
Ilkhomjon2604 <m347es@gmail.com>
2023-07-14 19:26:54 +0200

    all tasks done

--- array2/app.js
+++ array2/app.js
@@ -1,4 +1,41 @@
-let arr =[ 1, [ 3, [5]],[10] ];
-let sum = arr.flat(Infinity).reduce((int, total)=> int + +total, 0)
-console.log(sum);
+// Task 1
+
+// let arr =[ 1, [ 3, [5]],[10] ];
+// let sum = arr.flat(Infinity).reduce((int, total)=> int + +total, 0)
+// console.log(sum);
+
+
+
+// Task 2
+let cars = [
+    { id: 1 , year: 1998, engine: 1, name:'Tico' },
+    { id: 2 , year: 2005, engine: 1.2, name:'Matiz' },
+    { id: 3 , year: 2010, engine: 1.6, name:'Gentra' },
+    { id: 4 , year: 2010, engine: 1.5, name:'Cobalt' },
+    { id: 5 , year: 2012, engine: 2, name:'Malibu' },
+    { id: 6 , year: 2000, engine: 1.2, name:'Damas' },
+    { id: 7 , year: 2018, engine: 2.4, name:'Tracker' }
+];
+
+// let oldCars = cars.filter((value)=> value.year < 2000)
+// console.log(oldCars);
+
+// console.log(cars.map((value)=> value["engine"].sort()));
+// cars.map((value)=> {
+//     if (value.year< 2005)  value.status = 'Old' })
+// console.log(cars);
+
+
+
+
+// Task 3
+
+// let arr = [3, 2, 52, 12, 4, 34, 222]
+// console.log(Math.max(...arr), Math.min(...arr));
+
+// Task 4
+
+// let arr = ['apple', 'banana', 'melon', 'peach', 'pineapple', 'grenate']
+// let onDelete = (a) => arr.filter((value)=> value !== a) 
+// console.log(onDelete('banana'));
 
